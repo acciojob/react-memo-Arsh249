452285eba96c9aa937c3bde6fdaae2011d9e3157
Arsh Ilahi <arshilahi74@gmail.com>
2024-09-22 17:42:54 +0530

    q

--- src/components/App.js
+++ src/components/App.js
@@ -20,8 +20,8 @@ function App() {
         ))}
         <button id='add-todo-btn' onClick={addTodo}>Add Todo</button>
         <div>
-          <span id='incr-btn'>Count: {count+1}</span>
-          <button id='incr-cnt' onClick={() => setCount(count + 1)}>1</button>
+          <span id='incr-btn'>Count: {count}</span>
+          <button id='incr-cnt' onClick={() => setCount(count + 1)}>{count}</button>
         </div>
       </div>
 
